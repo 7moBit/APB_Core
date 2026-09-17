9599244236f03024b6c39271c0acb7bc2c283f66
DiegoMV7619 <diego.delamora7619@alumnos.udg.mx>
2026-09-16 18:41:59 -0600

    Parameters

--- rtl/apb_gpio_pkg.sv
+++ rtl/apb_gpio_pkg.sv
@@ -11,12 +11,12 @@ package apb_gpio_pkg;
         ADDR_INT_EDGE  = 8'h14  // R/W - per-bit edge(1)/level(0)
     } apb_gpio_addr_e;
 
-    typedef enum logic [7:0] 
-        COMPLETER0_BASE  = 8'h00;
-        COMPLETER0_LIMIT = 8'h80;   // inclusive upper bound
-        COMPLETER1_BASE  = 8'h80;   // inclusive lower bound
-        COMPLETER1_LIMIT = 8'hFF;
-    } apg_addr_memory_map;
+    parameter logic [7:0] COMPLETER0_BASE  = 8'h00;
+    parameter logic [7:0] COMPLETER0_LIMIT = 8'h80;   // inclusive upper bound
+
+    parameter logic [7:0] COMPLETER1_BASE  = 8'h80;   // inclusive lower bound
+    parameter logic [7:0] COMPLETER1_LIMIT = 8'hFF;
+
 
     typedef enum logic [1:0] {IDLE, SETUP, ACCESS} state_t;
  
